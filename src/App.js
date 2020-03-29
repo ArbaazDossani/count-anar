484274acc2dfb8947b27e9507987d76dfc226486
Arbaaz Dossani <arbaazdossani@macbooks-MacBook-Air.local>
2020-03-29 12:54:12 +0530

    CRA skeleton for anar counter

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      {
+        // TODO Anar Counter Code
+      } 
+    </div>
+  );
+}
+
+export default App;
