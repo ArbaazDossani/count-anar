416af019109ce90529e9d5e65ddcc0ad34479579
Arbaaz Dossani <arbaazdossani@macbooks-MacBook-Air.local>
2020-03-29 13:05:58 +0530

    Increment, Decrement and Click Counter usecase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [ counter, setCounter ] = useState(0);
+  const [ clickCount, setClickCount ] = useState(0);
   return (
     <div className="App">
-      {
-        // TODO Anar Counter Code
-      } 
+      <div>
+        Counter: {counter}
+      </div>
+      <div>
+        Number of Clicks: {clickCount}
+      </div>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+          setClickCount(clickCount + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCounter(counter > 0 ? counter - 1 : 0);
+          setClickCount(clickCount + 1);
+        }}
+      >
+        Decrement
+      </button> 
     </div>
   );
 }
